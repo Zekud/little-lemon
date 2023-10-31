b7c26a88c2c34c4cfab0ca93d7177ee51c8a0a27
zekud <zekudk@gmail.com>
2023-10-31 13:21:15 +0300

    trying to deploy

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter basename="/little-lemon">
+  <BrowserRouter basename="/">
     <App />
   </BrowserRouter>
 );
