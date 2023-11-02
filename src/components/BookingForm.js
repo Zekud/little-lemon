37b4e49dd45279d113e7f420d689f52ed4794b0a
zekud <zekudk@gmail.com>
2023-11-03 00:45:57 +0300

    editing the reservation

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -19,7 +19,7 @@ function DinnerErrorMessage() {
 
 function BookingForm() {
   const [formData, setFormData] = useState({
-    place: "",
+    place: null,
     date: "",
     dinners: "No Of Dinners",
     occassion: "select occassion",
@@ -56,16 +56,15 @@ function BookingForm() {
       ...formData,
       [name]: value,
     });
-    console.log(formData);
   };
-
+  const radio = document.getElementsByClassName("radio-input");
   const handleSubmit = (e) => {
     e.preventDefault();
     clearForm();
   };
   function clearForm() {
     setFormData({
-      place: "",
+      place: null,
       date: "",
       dinners: "No Of Dinners",
       occassion: "select occassion",
@@ -75,10 +74,12 @@ function BookingForm() {
     setDinnerTouched(false);
     setOccassionTouched(false);
     setTimeTouched(false);
+    radio[0].checked = false;
+    radio[1].checked = false;
   }
   function isValid() {
     return (
-      formData.place != "" &&
+      formData.place != null &&
       formData.date != "" &&
       formData.dinners != "No Of Dinners" &&
       formData.occassion != "select occassion" &&
@@ -98,112 +99,136 @@ function BookingForm() {
               <input
                 type="radio"
                 id="place1"
+                className="radio-input"
                 name="place"
                 value="indoor"
                 onChange={handleInputChange}
               />
             </div>
             <div className="each-place">
-              <label htmlFor="place2">Outdoor seating</label>
               <input
                 type="radio"
                 id="place2"
+                className="radio-input"
                 name="place"
                 value="outdoor"
                 onChange={handleInputChange}
               />
+              <label htmlFor="place2">Outdoor seating</label>
             </div>
           </div>
           <div id="date-dinner">
-            <div id="date-div">
-              <label htmlFor="date">Date</label>
-              <input
-                type="date"
-                id="date"
-                name="date"
-                value={formData.date}
-                onChange={handleInputChange}
-                onBlur={() => {
-                  setDateTouched(true);
-                }}
-              />
+            <div className="date-err">
+              <div id="date-div">
+                <label htmlFor="date">Date</label>
+                <input
+                  type="date"
+                  id="date"
+                  name="date"
+                  value={formData.date}
+                  className={formData.date !== "" ? "input-selected" : null}
+                  onChange={handleInputChange}
+                  onBlur={() => {
+                    setDateTouched(true);
+                  }}
+                />
+              </div>
               {dateTouched && formData.date == "" ? <DateErrorMessage /> : null}
             </div>
-            <div id="dinner">
-              <label htmlFor="dinners">Number of Dinners</label>
-              <select
-                id="dinners"
-                name="dinners"
-                value={formData.dinners}
-                onChange={handleInputChange}
-                onBlur={() => {
-                  setDinnerTouched(true);
-                }}
-              >
-                <option value="No Of Dinners" selected>
-                  No Of Dinners
-                </option>
-                {noOfDinners.map((dinner, index) => {
-                  return (
-                    <option key={index} value={dinner}>
-                      {dinner}
-                    </option>
-                  );
-                })}
-              </select>
+            <div className="dinner-err">
+              <div id="dinner">
+                <label htmlFor="dinners">Number of Dinners</label>
+                <select
+                  id="dinners"
+                  name="dinners"
+                  value={formData.dinners}
+                  className={
+                    formData.dinners !== "No Of Dinners"
+                      ? "input-selected"
+                      : null
+                  }
+                  onChange={handleInputChange}
+                  onBlur={() => {
+                    setDinnerTouched(true);
+                  }}
+                >
+                  <option value="No Of Dinners" defaultValue={true}>
+                    No Of Dinners
+                  </option>
+                  {noOfDinners.map((dinner, index) => {
+                    return (
+                      <option key={index} value={dinner}>
+                        {dinner}
+                      </option>
+                    );
+                  })}
+                </select>
+              </div>
               {dinnerTouched && formData.dinners == "No Of Dinners" ? (
                 <DinnerErrorMessage />
               ) : null}
             </div>
           </div>
           <div id="occ-time">
-            <div id="occ">
-              <label htmlFor="occassion">Occasion</label>
-              <select
-                id="occassion"
-                name="occassion"
-                value={formData.occassion}
-                onChange={handleInputChange}
-                onBlur={() => {
-                  setOccassionTouched(true);
-                }}
-              >
-                <option value="select occassion" selected>
-                  Select occassion
-                </option>
-                <option value="no special occassion">
-                  No special Occasion
-                </option>
-                <option value="Birthday">Birthday</option>
-                <option value="Anniversary">Anniversary</option>
-                <option value="Engagement">Engagement</option>
-              </select>
+            <div className="occ-err">
+              <div id="occ">
+                <label htmlFor="occassion">Occasion</label>
+                <select
+                  id="occassion"
+                  name="occassion"
+                  value={formData.occassion}
+                  className={
+                    formData.occassion !== "select occassion"
+                      ? "input-selected"
+                      : null
+                  }
+                  onChange={handleInputChange}
+                  onBlur={() => {
+                    setOccassionTouched(true);
+                  }}
+                >
+                  <option value="select occassion" defaultValue={true}>
+                    Select occassion
+                  </option>
+                  <option value="no special occassion">
+                    No special Occasion
+                  </option>
+                  <option value="Birthday">Birthday</option>
+                  <option value="Anniversary">Anniversary</option>
+                  <option value="Engagement">Engagement</option>
+                </select>
+              </div>
               {occassionTouched && formData.occassion == "select occassion" ? (
                 <OccasionErrorMessage />
               ) : null}
             </div>
-            <div id="time-div">
-              <label htmlFor="time">Time</label>
-              <select
-                id="time"
-                name="time"
-                value={formData.time}
-                onChange={handleInputChange}
-                onBlur={() => {
-                  setTimeTouched(true);
-                }}
-              >
-                <option value="select time" selected>
-                  Select Time
-                </option>
-                {availableTimes.map((time, index) => {
-                  return (
-                    <option key={index} value={time}>
-                      {time}
-                    </option>
-                  );
-                })}
-              </select>
+            <div className="time-err">
+              <div id="time-div">
+                <label htmlFor="time">Time</label>
+                <select
+                  id="time"
+                  name="time"
+                  value={formData.time}
+                  className={
+                    formData.time !== "select time" ? "input-selected" : null
+                  }
+                  onChange={handleInputChange}
+                  onBlur={() => {
+                    setTimeTouched(true);
+                  }}
+                >
+                  <option value="select time" defaultValue={true}>
+                    Select Time
+                  </option>
+                  {availableTimes.map((time, index) => {
+                    return (
+                      <option key={index} value={time}>
+                        {time}
+                      </option>
+                    );
+                  })}
+                </select>
+              </div>
               {timeTouched && formData.time == "select time" ? (
                 <TimeErrorMessage />
               ) : null}
@@ -216,9 +241,9 @@ function BookingForm() {
         <img src={img2} alt="chief" />
         <img src={img3} alt="food" />
       </div>
-      <div className="form-btn">
+      <div className="form-btn-div">
         <button
-          id="first-btn"
+          id="form-btn"
           disabled={!isValid()}
           className={isValid() ? "enabled" : "disabled"}
           onClick={handleSubmit}
